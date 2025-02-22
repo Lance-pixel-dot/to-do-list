b437cdcb60865c86add0902e1de2b2b78ad05111
Renier Lawrence Tuico <renierlawrencetuico143@gmail.com>
2025-02-22 12:35:39 +0800

    Create add 'task.js'.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import "./styles.css";
+import { createTask, appendTask } from "./add-task";
+
+const addTaskButton = document.querySelector(".add-task");
+
+addTaskButton.addEventListener("click", () => {
+    createTask();
+})
