9d4e000ba050be1f5c1932181ee3c4c1e300ab6b
Renier Lawrence Tuico <renierlawrencetuico143@gmail.com>
2025-06-23 19:00:24 +0800

    Updates

--- src/date-format.js
+++
@@ -1,8 +0,0 @@
-import { format, toDate } from 'date-fns';
-
-function formatDate(getDate){
-    const formattedDate = format(toDate(getDate), "M/d/yyyy");
-    return formattedDate;
-}
-
-export { formatDate };
